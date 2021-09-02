eb844ae82d8bfa2eeba23c96c1e463411328f81a
Willem Ruan <ruan.nieu@gmail.com>
2021-09-02 09:22:05 +0200

    webage Skeleton

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
 import './style.css';
+import mainPage from './initial';
+
+function start(){
+    const mainBody = document.querySelector("body");
+
+    //heading
+    const heading = document.createElement("div");
+    heading.setAttribute("id", "heading");
+    mainBody.appendChild(heading);
+
+    const title = document.createElement('h1');
+    title.textContent = "Peter William's Pie Palace"
+    title.classList.add("paragraphs");
+    heading.appendChild(title);
+
+    const list = document.createElement("ul");
+    heading.appendChild(list);
+    
+    const homeTab = document.createElement("li");
+    homeTab.textContent = "Home Tab";
+    list.appendChild(homeTab);
+
+    const menu = document.createElement("li");
+    menu.textContent = "Menu";
+    list.appendChild(menu);
+
+    const contact = document.createElement("li");
+    contact.textContent = "Home Tab";
+    list.appendChild(contact);
+
+    //body
+    const content = document.createElement("div");
+    content.setAttribute("id", "content");
+    mainBody.appendChild(content);
+};
+
+start();
+mainPage(content);
---
+++ src/initial.js
@@ -0,0 +1,18 @@
+function mainPage(div){
+    //background
+    const background = document.createElement("div");
+    
+    background.classList.add("background");
+
+    div.appendChild(background);
+
+    //text
+    const about = document.createElement("p");
+    
+    about.classList.add("paragraphs");  
+    about.textContent = "Welcome To Peter Williams Pie Palace! \r\n \r\n If you like pies then you've come to the right place!";
+
+    background.appendChild(about);
+};
+
+export default mainPage;
